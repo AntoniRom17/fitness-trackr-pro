eca7ffb78e9d34c028a12874b6950c0dffffbfcb
Antoni Roman <antonipaulo.roman@gmail.com>
2026-05-07 09:54:45 -0500

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import ActivityDetails from "./activities/ActivityDetails.jsx";
+
+import RoutinesPage from "./routines/RoutinesPage";
+import RoutineDetails from "./routines/RoutineDetails";
+
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Error404 from "./Error404";
-/**
- * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
- * discover new routines. Anyone can browse the site and make an account, and users with an
- * account will be able to upload and manage their own activities.
- */
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Layout route */}
         <Route path="/" element={<Layout />}>
+          
           {/* Public routes */}
           <Route index element={<ActivitiesPage />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
 
-          {/* Dynamic activity page */}
+          {/* Activities */}
           <Route path="activities/:id" element={<ActivityDetails />} />
+
+          {/* Routines */}
+          <Route path="routines" element={<RoutinesPage />} />
+          <Route path="routines/:id" element={<RoutineDetails />} />
 
           {/* Catch-all */}
           <Route path="*" element={<Error404 />} />
